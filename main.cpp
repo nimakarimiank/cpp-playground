aa35d4281c590d21c2043e09656243c1fd40d9c5
nima <niimak97@gmail.com>
2024-07-27 23:02:58 +0330

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//libraries
+#include <iostream>
+//
+//here is the starting point
+int main() {
+    int a,b;
+    int result;
+    a = 12;
+    b= 3;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+
+    return 0;
+}
