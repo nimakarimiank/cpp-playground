d97684fa3f5ef11ffc898e1a0f1766081193ec41
nima <niimak97@gmail.com>
2024-12-06 01:16:14 +0330

    init

--- main.cpp
+++ main.cpp
@@ -2,12 +2,21 @@
    Created by nima karimian on 06.12.24.
 */
 #include <iostream>
+
 #if defined(__cpp_lib_ranges) && __cplusplus >= 202300L
 #define CPP23_SUPPORTED true
 #else
 #define CPP23_SUPPORTED false
 #endif
-int main(){
+
+namespace nima {
+    void print_helloworld() {
+        std::println("hi");
+    }
+}
+int main() {
+    nima::print_helloworld();
     std::cout << "C++23 support: " << (CPP23_SUPPORTED ? "Yes" : "No") << std::endl;
     return 0;
 }
+
